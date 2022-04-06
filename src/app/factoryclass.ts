7b65de59ffe629129fb2025f8309cf0bdec66e21
Sneha Chandran <Sneha.Chandran@dh.com>
2022-04-06 10:25:54 +0530

    Add project files.

---
+++ src/app/factoryclass.ts
@@ -0,0 +1,12 @@
+export class Factoryclass {
+    public Userid!:Number;
+    public FirstName!:String;
+    public Location!:String;
+    public Age!:Number;
+    public FullName!:String;
+    public TaxID!:String;
+    public BusinessType!:String;
+    public AnnualIncome!:Number;
+    public Email!:String;
+    public PhoneNumber!:Number;
+}
